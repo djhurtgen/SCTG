9c03c75c5d50f89538d411d74a214a01036232ea
djhurtgen <dhurtgen@fredonia.edu>
2022-03-06 16:22:44 -0500

    with congestion calculation

--- _include/Graph.h
+++ _include/Graph.h
@@ -4,9 +4,9 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <iomanip>
 #include "Road.h"
 #include "Intersection.h"
-using namespace std;
 
 const int inf = 1000;
 const int no_intersections = 16;
@@ -14,13 +14,14 @@ const int no_intersections = 16;
 template<class V>
 class Graph
 {
-	vector<Intersection<V>*> adjListV;			//vector of Nodes									
-	int adj_matrix[no_intersections][no_intersections];	//2D mapping of congestion at intersections
+	vector<Intersection<V>*> adjListV;						//vector of Nodes									
+	float adj_matrix[no_intersections][no_intersections];	//2D mapping of congestion at intersections
 
 public:
 	//Constructor (populates graph)
 	Graph(vector<Road<V>> const &roads);
-	int findLeastCongestedIntersection(bool[], int[]);
+	int findLeastCongestedIntersection(bool[], float[]);
+	float calculateCF(const int&, const float&, const int&);
 	void calculateSP(const V&, const V&);
 	void printAdjList() const;
 	void printAdjMatrix() const;
@@ -36,20 +37,21 @@ Graph<V>::Graph(vector<Road<V>> const &roads) {
 		bool nodeExists = false;
 		V src = road.getSrc();
 		V dest = road.getDst();
-		int weight = road.getWeight();
-		cout << "adding: " << src << "----w(" << weight << ")----" << dest << endl;
+		//calculate here
+		float weight = calculateCF(road.getNumCars(), road.getNumMiles(), road.getAvgSpeed());
+		std::cout << "adding: " << src << "----w(" << weight << ")----" << dest << std::endl;
 		
 
 		if (!adjListV.empty()) {
-			for (Intersection<V> *node : adjListV) {		//for every Node in the adjacency list
+			for (Intersection<V> *node : adjListV) {		
 				if (src == node->getIntersectionValue()) {
-					nodeExists = true;				//if Node already exists, assign "true" and break
+					nodeExists = true;						
 					break;							
 				}
 			}
 		}
 
-		cout << src << (nodeExists ? " exists" : " does not exist") << endl;
+		std::cout << src << (nodeExists ? " exists" : " does not exist") << std::endl;
 
 		//modify this to create priority queue
 		if (nodeExists) {
@@ -75,17 +77,23 @@ Graph<V>::Graph(vector<Road<V>> const &roads) {
 			adjListV.push_back(new Intersection<V>(src));
 			Intersection<V> *curr = adjListV.at(src);
 			curr->setNextIntersection(new Intersection<V>(dest, weight));
-			cout << "new node and destination added." << endl;
+			std::cout << "new node and destination added." << std::endl;
 		}
 	}
 }
 
+template<class V>
+float Graph<V>::calculateCF(const int& c, const float& m, const int& v) {
+	float cf;
+	cf = (c / (352 * m)) / v;
+	return cf;
+}
 
 template<class V>
 void Graph<V>::calculateSP(const V& s, const V& t) {
 
 	bool visited[no_intersections];		//array of boolean values... visited or not
-	int distance[no_intersections];		//array of distances, which are weights in this case
+	float distance[no_intersections];		//array of distances, which are weights in this case
 
 	//init adj_matrix, adj = 0, all others at inf
 	for (int i = 0; i < no_intersections; i++)
@@ -95,7 +103,7 @@ void Graph<V>::calculateSP(const V& s, const V& t) {
 
 	//populate adj_matrix from adjListV with weight values
 	if (!adjListV.empty()) {
-		for (Intersection<V> *node : adjListV) {	//for each Node in adjListV
+		for (Intersection<V> *node : adjListV) {	
 			Intersection<V> *curr = node;
 
 			while(curr->getNextIntersection() != nullptr) {		//traverse its linked list and insert weights
@@ -120,10 +128,10 @@ void Graph<V>::calculateSP(const V& s, const V& t) {
 	for (int count = 0; count < no_intersections; count++){
 
 		int v = findLeastCongestedIntersection(visited, distance);		//v is to be added next
-		visited[v] = true;											//add v to visited nodes
+		visited[v] = true;												//add v to visited nodes
 
 		if (v == t) {
-			cout << "Lowest cost path from " << s << " to " << t << ": " << distance[v] << endl;
+			std::cout << "Lowest cost path from " << s << " to " << t << ": " << distance[v] << std::endl;
 			break;
 		}
 
@@ -137,7 +145,7 @@ void Graph<V>::calculateSP(const V& s, const V& t) {
 
 
 template<class V>
-int Graph<V>::findLeastCongestedIntersection(bool visited[], int distance[]) {
+int Graph<V>::findLeastCongestedIntersection(bool visited[], float distance[]) {
 		int min = inf, smallest_weight_node;		//same as int min = inf; int smallest_weight_node;
 		for (int i = 0; i < no_intersections; i++){
 			if (!visited[i] && distance[i] <= min){
@@ -152,17 +160,17 @@ int Graph<V>::findLeastCongestedIntersection(bool visited[], int distance[]) {
 // print adjacency list representation of graph
 template<class V>
 void Graph<V>::printAdjList() const {
-	cout << endl << "Adjacency list..." << endl;
+	std::cout << std::endl << "Adjacency list..." << endl;
 	for (Intersection<V> *node : adjListV) {
 		// print all neighboring vertices of given vertex
 		Intersection<V> *curr = node;
-		cout << curr->getIntersectionValue(); //print graph node
+		std::cout << curr->getIntersectionValue();			//print graph node
 		curr = curr->getNextIntersection();
 		while (curr != nullptr){
-			cout << " --> [" << curr->getIntersectionValue() << ",w(" << curr->getIntersectionWeight() << ")]"; //print adjacent nodes to graph node
-			curr = curr->getNextIntersection(); //move to next adjacent node
+			std::cout << " --> [" << curr->getIntersectionValue() << ",w(" << curr->getIntersectionWeight() << ")]"; //print adjacent nodes to graph node
+			curr = curr->getNextIntersection();			//move to next adjacent node
 		}
-		cout << endl;
+		std::cout << std::endl;
 	}
 }
 
@@ -170,19 +178,19 @@ void Graph<V>::printAdjList() const {
 // print adjacency matrix representation of graph
 template<class V>
 void Graph<V>::printAdjMatrix() const {
-	cout << endl << "Adjacency matrix..." << endl;
-	cout << "\t";
-	for (int i = 0; i < no_intersections; i++) {		//column headings are printed (0 - 4)
-		cout << i << "\t";
+	std::cout << endl << "Adjacency matrix..." << std::endl;
+	std::cout << "\t";
+	for (int i = 0; i < no_intersections; i++) {		//column headings are printed
+		std::cout <<  i << "\t";
 	}
-	cout << endl;
+	std::cout << endl;
 	for (int i = 0; i < no_intersections; i++) {
-		cout << i << "\t";						//row headings (0 - 4)
-		for (int j = 0; j < no_intersections; j++) {	//matrix values
-			if (adj_matrix[i][j] == inf) cout << static_cast<unsigned char>(236) << "\t";
-			else cout << adj_matrix[i][j] << "\t";
+		std::cout << i << "\t";								//row headings
+		for (int j = 0; j < no_intersections; j++) {		//matrix values
+			if (adj_matrix[i][j] == inf) std::cout << static_cast<unsigned char>(236) << "\t";
+			else std::cout << std::setprecision(2) << adj_matrix[i][j] << "\t";
 		}
-		cout << endl;
+		std::cout << endl;
 	}		
 }
 
--- _include/Intersection.h
+++ _include/Intersection.h
@@ -6,21 +6,21 @@
 template<class V>
 class Intersection {
     V value;
-	int weight;
+	float weight;
     Intersection<V> *next;
 
 public:
     Intersection():value(nullptr), weight(0), next(nullptr) {};
     Intersection(V& val) :value(val), weight(0), next(nullptr) {};
-    Intersection(V& val, int W):value(val), weight(W), next(nullptr) {};
-    Intersection(V& val, int W, Intersection<V> *nextIntersection):value(val), weight(W), next(nextIntersection) {};
+    Intersection(V& val, float W):value(val), weight(W), next(nullptr) {};
+    Intersection(V& val, float W, Intersection<V> *nextIntersection):value(val), weight(W), next(nextIntersection) {};
     
     Intersection<V>* getNextIntersection() const;
     void setNextIntersection(Intersection<V>*);
     V getIntersectionValue() const;
     void setIntersectionValue(V);
-	V getIntersectionWeight() const;
-	void setIntersectionWeight(int);
+	float getIntersectionWeight() const;
+	void setIntersectionWeight(float);
 };
 
 template<class V>
@@ -44,12 +44,12 @@ void Intersection<V>::setIntersectionValue(V val){
 }
 
 template<class V>
-V Intersection<V>::getIntersectionWeight() const {
+float Intersection<V>::getIntersectionWeight() const {
 	return weight;
 }
 
 template<class V>
-void Intersection<V>::setIntersectionWeight(int val) {
+void Intersection<V>::setIntersectionWeight(float val) {
 	weight = val;
 }
 
